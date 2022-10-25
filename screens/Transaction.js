fd3c54c5203e2b3146eec6f441e2ee1e9cdbf36f
maxg1007 <90800419+maxg1007@users.noreply.github.com>
2022-10-25 15:44:45 -0300

    Add files via upload

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -1,56 +1,197 @@
-import { Component } from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React, { Component } from "react";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  ImageBackground,
+  Image,
+} from "react-native";
 import * as Permissions from "expo-permissions";
 import { BarCodeScanner } from "expo-barcode-scanner";
+import db from "../config";
+
+const bgImage = require("../assets/background2.png");
+const appIcon = require("../assets/appIcon.png");
+const appName = require("../assets/appName.png");
 
 export default class TransactionScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      bookId: "",
+      studentId: "",
       domState: "normal",
       hasCameraPermissions: null,
-      scaned: false,
-      scanedData: "",
+      scanned: false,
     };
   }
+  handleTransaction = () => {
+    var { bookId } = this.state;
+    db.collection("BSC001")
+      .doc(bookId)
+      .get()
+      .then((doc) => {
+        var book = doc.data();
+        if (book.is_book_available) {
+          this.initiateBookIssue();
+        } else {
+          this.initiateBookReturn();
+        }
+      });
+  };
+  initiateBookIssue = () => {
+    console.log("livro retirado");
+  };
+  initiateBookReturn = () => {
+    console.log("livro devolvido");
+  };
+
   getCameraPermissions = async (domState) => {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
+
     this.setState({
+      /*status === "granted" é verdadeiro se o usuário concedeu permissão
+          status === "granted" é falso se o usuário não concedeu permissão
+        */
       hasCameraPermissions: status === "granted",
       domState: domState,
-      scaned: false,
+      scanned: false,
     });
   };
-  handleBarCodeScaned = async ({ type, data }) => {
-    this.setState({
-      scanedData: data,
-      scaned: true,
-      domState: "normal",
-    });
+
+  handleBarCodeScanned = async ({ type, data }) => {
+    const { domState } = this.state;
+
+    if (domState === "bookId") {
+      this.setState({
+        bookId: data,
+        domState: "normal",
+        scanned: true,
+      });
+    } else if (domState === "studentId") {
+      this.setState({
+        studentId: data,
+        domState: "normal",
+        scanned: true,
+      });
+    }
   };
+
   render() {
-    const { domState, hasCameraPermissions, scaned, scanedData } = this.state;
-    if (domState === "scaner") {
+    const { bookId, studentId, domState, scanned } = this.state;
+    if (domState !== "normal") {
       return (
         <BarCodeScanner
-          onBarCodeScanned={scaned ? undefined : this.handleBarCodeScaned}
+          onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
           style={StyleSheet.absoluteFillObject}
-        ></BarCodeScanner>
+        />
       );
     }
     return (
-      <View>
-        <Text>
-          {hasCameraPermissions ? scanedData : "Solicitar permissao da camera."}
-        </Text>
-        <TouchableOpacity
-          onPress={() => {
-            this.getCameraPermissions("scaner");
-          }}
-        >
-          <Text>Escanear QR code</Text>
-        </TouchableOpacity>
+      <View style={styles.container}>
+        <ImageBackground source={bgImage} style={styles.bgImage}>
+          <View style={styles.upperContainer}>
+            <Image source={appIcon} style={styles.appIcon} />
+            <Image source={appName} style={styles.appName} />
+          </View>
+          <View style={styles.lowerContainer}>
+            <View style={styles.textinputContainer}>
+              <TextInput
+                style={styles.textinput}
+                placeholder={"ID do Livro"}
+                placeholderTextColor={"#FFFFFF"}
+                value={bookId}
+              />
+              <TouchableOpacity
+                style={styles.scanbutton}
+                onPress={() => this.getCameraPermissions("bookId")}
+              >
+                <Text style={styles.scanbuttonText}>Digitalizar</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={[styles.textinputContainer, { marginTop: 25 }]}>
+              <TextInput
+                style={styles.textinput}
+                placeholder={"ID do Estudante"}
+                placeholderTextColor={"#FFFFFF"}
+                value={studentId}
+              />
+              <TouchableOpacity
+                style={styles.scanbutton}
+                onPress={() => this.getCameraPermissions("studentId")}
+              >
+                <Text style={styles.scanbuttonText}>Digitalizar</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ImageBackground>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+  },
+  bgImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  upperContainer: {
+    flex: 0.5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  appIcon: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+    marginTop: 80,
+  },
+  appName: {
+    width: 180,
+    resizeMode: "contain",
+  },
+  lowerContainer: {
+    flex: 0.5,
+    alignItems: "center",
+  },
+  textinputContainer: {
+    borderWidth: 2,
+    borderRadius: 10,
+    flexDirection: "row",
+    backgroundColor: "#9DFD24",
+    borderColor: "#FFFFFF",
+  },
+  textinput: {
+    width: "57%",
+    height: 50,
+    padding: 10,
+    borderColor: "#FFFFFF",
+    borderRadius: 10,
+    borderWidth: 3,
+    fontSize: 18,
+    backgroundColor: "#5653D4",
+    fontFamily: "Rajdhani_600SemiBold",
+    color: "#FFFFFF",
+  },
+  scanbutton: {
+    width: 100,
+    height: 50,
+    backgroundColor: "#9DFD24",
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  scanbuttonText: {
+    fontSize: 20,
+    color: "#0A0101",
+    fontFamily: "Rajdhani_600SemiBold",
+  },
+});
